269607cd7930e955af2ad7d53f3dfb32bba7479f
KachanDaniil <daniel.meadjohnson1994@gmail.com>
2019-06-19 09:39:35 +0300

    lab3

--- src/kachanlab3/Main.java
+++ src/kachanlab3/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kachanlab2;
+package kachanlab3;
 
 /**
  *
@@ -12,28 +12,34 @@ package kachanlab2;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /**
- * Вычисление и отображение результатов. Содержит реализацию статического метода
- * main().
+ * Вычисление и отображение результатов<br>
+ * Содержит реализацию статического метода main()
  *
  * @author xone
- * @version 1.0
+ * @version 2.0
  * @see Main#main
  */
 public class Main {
 
     /**
-     * Объект класса {@linkplain Calc}.<br>Решает задачу инд. задания.
+     * Объект, реализующий интерфейс {@linkplain View}; обслуживает коллекцию
+     * объектов {@linkplain ex01.Item2d}
      */
-    private Calc calc = new Calc();
-    Scanner reader = new Scanner(System.in);
+    private View view;
+
+    /**
+     * Инициализирует поле {@linkplain Main#view view}.
+     */
+    public Main(View view) {
+        this.view = view;
+    }
 
     /**
      * Отображает меню.
      */
-    private void menu() {
+    protected void menu() {
         String s = null;
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         do {
@@ -53,45 +59,45 @@ public class Main {
                     break;
                 case 'v':
                     System.out.println("View current.");
-                    calc.show();
+                    view.viewShow();
                     break;
                 case 'e':
                     System.out.println("Enter integer number.");
-                    calc.init(reader.nextInt());
-                    calc.show();
+                    view.viewInit();
+                    view.viewShow();
                     break;
                 case 's':
                     System.out.println("Save current.");
                     try {
-                        calc.save();
+                        view.viewSave();
                     } catch (IOException e) {
                         System.out.println("Serialization error: " + e);
                     }
-                    calc.show();
+                    view.viewShow();
                     break;
                 case 'r':
                     System.out.println("Restore last saved.");
                     try {
-                        calc.restore();
+                        view.viewRestore();
                     } catch (Exception e) {
                         System.out.println("Serialization error: " + e);
                     }
-                    calc.show();
+                    view.viewShow();
                     break;
                 default:
-                    System.out.print("Wrong command. ");
+                    System.out.println("Wrong command.");
             }
         } while (s.charAt(0) != 'q');
     }
 
     /**
-     * Выполняется при запуске программы. Вычисляется значение функции для
-     * различных аргументов. Результаты вычислений выводятся на экран.
+     * Выполняется при запуске программы; вызывает метод
+     * {@linkplain Main#menu() menu()}
      *
      * @param args - параметры запуска программы.
      */
     public static void main(String[] args) {
-        Main main = new Main();
+        Main main = new Main(new ViewableResult().getView());
         main.menu();
     }
 }
